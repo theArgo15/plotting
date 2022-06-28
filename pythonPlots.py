feb7ff0d473b27e3012aab6d9a4d256716b4fa5b
theArgo15 <tony@tonywitt.com>
2022-06-28 09:50:38 -0500

    Add simple plot

---
+++ pythonPlots.py
@@ -0,0 +1,23 @@
+from cProfile import label
+from time import time
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+overallTitle = 'Ep Temperatures Over Time'
+svp2Job = pd.read_excel('1656084383_appLogs.xlsx')
+
+
+
+fig, ax = plt.subplots()
+ax.plot('Time', 'EPTemp', data=svp2Job, label='EPTemp')
+ax.plot('Time', 'ECSReturnTemp', data=svp2Job, label='ECSReturnTemp')
+
+ax.set_xlabel('Time (s)')
+ax.set_ylabel('Temperature ($^\circ$C)')
+ax.set_title(overallTitle)
+ax.legend()
+
+if __name__ == "__main__":
+    plt.show(block=True)
