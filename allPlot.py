21e520d6a6b6e87705bc32d31a4d44219d1504b0
theArgo15 <tony@tonywitt.com>
2022-07-06 10:04:24 -0500

    First Release

--- allPlot.py
+++ allPlot.py
@@ -3,29 +3,44 @@ from lib2to3.pgen2.pgen import DFAState
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from pathlib import Path
 
 def tempPlotter(df):
     fig, ax = plt.subplots()
+    colormap = plt.cm.nipy_spectral
+    colors = [colormap(i) for i in np.linspace(0, 1,21)]
+    ax.set_prop_cycle('color', colors)
     for column in df.columns:
         if 'Temp' in column:
-            ax.plot('Time', column, data=df, label=column)
+            if 'Logger' in column:
+                ax.plot(f'Logger{column[6]}Time', column, data=df, label=column)
+            else:
+                ax.plot('Time', column, data=df, label=column)
     ax.set_xlabel('Time (s)')
     ax.set_ylabel('Temperature ($^\circ$C)')
-    ax.set_title('Title')
+    ax.set_title('Temperature Over Time')
     ax.legend()
+
 
 def RHPlotter(df):
         fig, ax = plt.subplots()
+        colormap = plt.cm.nipy_spectral
+        colors = [colormap(i) for i in np.linspace(0, 1,21)]
+        ax.set_prop_cycle('color', colors)
         for column in df.columns:
             if "Humidity" in column:
-                ax.plot('Time', column, data=df, label=column)
+                if 'Logger' in column:
+                    ax.plot(f'Logger{column[6]}Time', column, data=df, label=column)
+                else:
+                    ax.plot('Time', column, data=df, label=column)
         ax.set_xlabel('Time (s)')
         ax.set_ylabel('Relative Humidity (%)')
-        ax.set_title('Title')
+        ax.set_title('Humidity Over Time')
         ax.legend()
     
 
 if __name__ == "__main__":
-    tempPlotter(pd.read_csv('newNewData.csv'))
-    RHPlotter(pd.read_csv('newNewData.csv'))
+    filePath = filePath = Path(r'C:\Users\TonyWitt\OneDrive - Evolve\Documents\Flir Logs\30Jun')
+    #tempPlotter(pd.read_csv(filePath/'syncedData.csv'))
+    RHPlotter(pd.read_csv(filePath/'syncedData.csv'))
     plt.show(block=True)
--- timeSync.py
+++ timeSync.py
@@ -4,25 +4,31 @@ from datetime import datetime
 from pathlib import Path
 
 #start time in unix timestamp is the first part of the title of the xl sheet
-jobStartTime = 1656084383
+jobStartTime = 1656605942
+#File path will need to be updated for each folder of data
+filePath = Path(r'C:\Users\TonyWitt\OneDrive - Evolve\Documents\Flir Logs\30Jun')
 #import job data into dataframe
-internalData = pd.read_excel(f'{str(jobStartTime)}_appLogs.xlsx')
-#import all the logger data into dataframe
-#with 18 temp loggers, this might be a loop of some sort
-filePath = Path(r'C:\Users\TonyWitt\OneDrive - Evolve\Documents\Flir Logs')
-externalData = pd.read_csv(filePath/'Logger1.xls', skiprows=7, sep='\t', parse_dates=[['DATE','TIME']])
-
+internalData = pd.read_excel(filePath/f'{str(jobStartTime)}_appLogs.xlsx')
 
 #Create and save dataframe of data we care about from logger and internal data
-svp2Job = internalData.loc[:,('Time','EPTemp', 'EPHumidity','ECSReturnTemp', 'ECSReturnHumidity','ECSSupplyTemp','ECSSupplyHumidity')]
+SVP2Job = pd.DataFrame(index=range(16000),columns=range(1))
+svp2Job = pd.concat([SVP2Job, internalData.loc[:,('Time','EPTemp', 'EPHumidity','ECSReturnTemp', 'ECSReturnHumidity','ECSSupplyTemp','ECSSupplyHumidity')]],axis=1)
 # #get a column of unix time
-# svp2Job['ECSSupplyTemp'] = externalData['ECSSupplyTemp']
 svp2Job['TimeInEpoc'] = svp2Job['Time'] + jobStartTime
 svp2Job['SVPTime'] = pd.to_datetime(svp2Job['TimeInEpoc'], unit='s') 
-# Add column for each logger time, logger temp, logger RH
+#import all the logger data into dataframe
+#with 18 temp loggers, this might be a loop of some sort
 
+for i in range(18):
+    # Add column for each logger time, logger temp, logger RH
+    # Logger time starts as datetime. Convert into epoch time by subtracting jan 1 1970 and displaying total seconds, then subtract jobStartTime to get synced time
+    externalData = pd.read_csv(filePath/f'Logger{i+1}.xls', skiprows=7, sep='\t', parse_dates=[['DATE','TIME']])
+    svp2Job[f'Logger{i+1}DateTime'] = externalData['DATE_TIME'].dt.tz_localize(tz='US/Central')
+    svp2Job[f'Logger{i+1}Time'] =  svp2Job[f'Logger{i+1}DateTime'].astype('int64')//1e9 - jobStartTime
+    svp2Job[f'Logger{i+1}Temp'] = externalData['TEMPERATURE']
+    svp2Job[f'Logger{i+1}Humidity'] = externalData['RELATIVE-HUMIDITY']
 ### Drop the old table.
 #svp2Job.drop('Time', axis = 1, inplace = True)
 #write out new table
-svp2Job.to_csv('newNewData.csv', index = False)
+svp2Job.to_csv(filePath/'syncedData.csv', index = False)
 pass
