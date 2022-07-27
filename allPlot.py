4ec5f20c62f0dca66bb039b522ff13d437dda871
theArgo15 <tony@tonywitt.com>
2022-07-27 12:59:38 -0500

    Add csv files for metadata

--- allPlot.py
+++ allPlot.py
@@ -8,14 +8,15 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from pathlib import Path
+import referenceVariable
 
-def featurePlotter(df, feature):
+def attributePlotter(df, attribute):
     fig, ax = plt.subplots()
     colormap = plt.cm.nipy_spectral
     colors = [colormap(i) for i in np.linspace(0, 1,22)]
     ax.set_prop_cycle('color', colors)
     for column in df.columns:
-        if feature in column:
+        if attribute in column:
             if 'Logger' in column:
                 #loggers greater than 9 are using logger 1 time
                 if len(column) == 11:
@@ -29,53 +30,52 @@ def featurePlotter(df, feature):
             else:
                 ax.plot('Time', column, data=df, label=column)
     ax.set_xlabel('Time (s)')
-    ax.set_ylabel(f'{feature} ($^\circ$C) or (%)')
+    ax.set_ylabel(f'{attribute} ($^\circ$C) or (%)')
     ax.set_title('Control')
     ax.legend()
 
 
 
-def splitFeaturePlotter(df, feature):
+def splitAttributePlotter(df, attribute):
     #6 total graphs: 4 engines, EP bay, SVP tracked stuff
     fig, axs = plt.subplots(3,2, sharex=True, sharey=True)
     jobFinishTime = df['Time'].max()
     for i in range(4):
-        axs[0, 0].plot(f'Logger{i+1}Time', f'Logger{i+1}{feature}', data=df, label = f'Logger{i+1}')
-        axs[0, 1].plot(f'Logger{i+5}Time', f'Logger{i+5}{feature}', data=df, label = f'Logger{i+5}')
-        axs[1, 0].plot(f'Logger{i+9}Time', f'Logger{i+9}{feature}', data=df, label = f'Logger{i+9}')
-        axs[1, 1].plot(f'Logger{i+13}Time', f'Logger{i+13}{feature}', data=df, label = f'Logger{i+13}')
+        axs[0, 0].plot(f'Logger{i+1}Time', f'Logger{i+1}{attribute}', data=df, label = referenceVariable.LocationDescriptions.loc[f'Logger{i+1}', 'Location Position'])
+        axs[0, 1].plot(f'Logger{i+5}Time', f'Logger{i+5}{attribute}', data=df, label = referenceVariable.LocationDescriptions.loc[f'Logger{i+5}', 'Location Position'])
+        axs[1, 0].plot(f'Logger{i+9}Time', f'Logger{i+9}{attribute}', data=df, label = referenceVariable.LocationDescriptions.loc[f'Logger{i+9}', 'Location Position'])
+        axs[1, 1].plot(f'Logger{i+13}Time', f'Logger{i+13}{attribute}', data=df, label = referenceVariable.LocationDescriptions.loc[f'Logger{i+13}', 'Location Position'])
     for i in range(3):
         try:
-            axs[2, 0].plot(f'Logger{i+17}Time', f'Logger{i+17}{feature}', data=df, label = f'Logger{i+17}')
+            axs[2, 0].plot(f'Logger{i+17}Time', f'Logger{i+17}{attribute}', data=df, label = referenceVariable.LocationDescriptions.loc[f'Logger{i+17}', 'Location Position'])
         except:
             raise Warning('Logger 19 not found')
-    #some terrible logic for getting ECS supply, return, and EP feature 
+    #some terrible logic for getting ECS supply, return, and EP attribute 
     for column in df.columns:
-        if feature in column:
+        if attribute in column:
             if 'Logger' in column:
                 pass
             else:
                 axs[2,1].plot('Time', column, data=df, label=column)
 
-    plotTitles = ['Engine 1', 'Engine 2', 'Engine 3', 'Engine 4', 'Electronics Bay', 'SVP Tracked Data']
-    featureUnits = {'Temp': '($^\circ$C)', 'Humidity': '(%)'}
+    plotTitles = ['Engine 1', 'Engine 2', 'Engine 3', 'Engine 4', 'Misc Locations', 'SVP Tracked Data']
     for i, ax in enumerate(axs.reshape(-1)):
         ax.axvline(x=jobFinishTime)
         ax.set_title(plotTitles[i])
         ax.legend()
         ax.set_xlabel('Time (s)')
-        ax.set_ylabel(feature + featureUnits[feature])
+        ax.set_ylabel(attribute + referenceVariable.attributeUnits[attribute])
     return jobFinishTime
 
-def PositionSplitFeaturePlotter(df, feature):
+def PositionSplitAttributePlotter(df, attribute):
     fig, axs = plt.subplots(2,2, sharex=True, sharey=True)
     jobFinishTime = df['Time'].max()
     every4 =[1,5,7,11]
     for LoggerLabel in every4:
-        axs[0, 0].plot(f'Logger{LoggerLabel}Time', f'Logger{LoggerLabel}{feature}', data=df, label = f'Logger{LoggerLabel}')
-        axs[0, 1].plot(f'Logger{LoggerLabel+1}Time', f'Logger{LoggerLabel+1}{feature}', data=df, label = f'Logger{LoggerLabel+1}')
-        axs[1, 0].plot(f'Logger{LoggerLabel+2}Time', f'Logger{LoggerLabel+2}{feature}', data=df, label = f'Logger{LoggerLabel+2}')
-        axs[1, 1].plot(f'Logger{LoggerLabel+3}Time', f'Logger{LoggerLabel+3}{feature}', data=df, label = f'Logger{LoggerLabel+3}')
+        axs[0, 0].plot(f'Logger{LoggerLabel}Time', f'Logger{LoggerLabel}{attribute}', data=df, label = f'Logger{LoggerLabel}')
+        axs[0, 1].plot(f'Logger{LoggerLabel+1}Time', f'Logger{LoggerLabel+1}{attribute}', data=df, label = f'Logger{LoggerLabel+1}')
+        axs[1, 0].plot(f'Logger{LoggerLabel+2}Time', f'Logger{LoggerLabel+2}{attribute}', data=df, label = f'Logger{LoggerLabel+2}')
+        axs[1, 1].plot(f'Logger{LoggerLabel+3}Time', f'Logger{LoggerLabel+3}{attribute}', data=df, label = f'Logger{LoggerLabel+3}')
 
     for i, ax in enumerate(axs.reshape(-1)):
         ax.axvline(x=jobFinishTime)
@@ -84,20 +84,21 @@ def PositionSplitFeaturePlotter(df, feature):
     return jobFinishTime
 
 if __name__ == "__main__":
-    dataFolder = '30Jun'
-    filePath = filePath = Path(r'C:\Users\TonyWitt\OneDrive - Evolve\Documents\Flir Logs')
-    June30 = filePath/dataFolder
-    July7 = filePath/'06Jul07Jul'
+    jobsData = pd.read_csv('jobsData.csv', index_col='Date')
+    #input
+    job = 'July26'
 
-
-    job = July7
+    job = jobsData.loc[job, 'Path']
+    job = Path(job)
     dataLocation = pd.read_csv(job/'syncedData.csv')
     
-    #featurePlotter(dataLocation,'Temp')
-    #jobFinishTime = PositionSplitFeaturePlotter(dataLocation, 'Temp')
-    jobFinishTime = splitFeaturePlotter(dataLocation, 'Humidity')
+    #attributePlotter(dataLocation,'Temp')
+    #jobFinishTime = PositionSplitAttributePlotter(dataLocation, 'Temp')
+    jobFinishTime = splitAttributePlotter(dataLocation, 'Humidity')
     plt.xlim([0,jobFinishTime+900])
     #plt.ylim([20,27])
-    plt.suptitle('07Jul Control Data')
+    #Limits for RH
+    plt.ylim([20,50])
+    plt.suptitle('26Jul Data (Sheetmetal Shroud)')
     plt.show(block=True)
-    #plt.savefig(Path(r"C:\Users\TonyWitt\OneDrive - Evolve\Pictures")/str(job)[-5:])
+    #plt.savefig(Path(r"C:\Users\TonyWitt\OneDrive - Evolve\Pictures")/'test')
--- timeSync.py
+++ timeSync.py
@@ -14,27 +14,33 @@ def timeSync(jobStartTime, filePath):
     svp2Job['TimeInEpoc'] = svp2Job['Time'] + jobStartTime
     svp2Job['SVPTime'] = pd.to_datetime(svp2Job['TimeInEpoc'], unit='s') 
     #import all the logger data into dataframe
-    #with 18 temp loggers, this might be a loop of some sort
+    #with 19 temp loggers, this might be a loop of some sort
 
-    for i in range(18):
+    for i in range(19):
         # Add column for each logger time, logger temp, logger RH
         # Logger time starts as datetime. Convert into epoch time by subtracting jan 1 1970 and displaying total seconds, then subtract jobStartTime to get synced time
-        externalData = pd.read_csv(filePath/f'Logger{i+1}.xls', skiprows=7, sep='\t', parse_dates=[['DATE','TIME']])
+        externalData = pd.read_csv(filePath/f'Logger{i+1}.txt', skiprows=7, sep='\t', parse_dates=[['DATE','TIME']])
         svp2Job[f'Logger{i+1}DateTime'] = externalData['DATE_TIME'].dt.tz_localize(tz='US/Central')
         svp2Job[f'Logger{i+1}Time'] =  svp2Job[f'Logger{i+1}DateTime'].astype('int64')//1e9 - jobStartTime
         svp2Job[f'Logger{i+1}Temp'] = externalData['TEMPERATURE']
         svp2Job[f'Logger{i+1}Humidity'] = externalData['RELATIVE-HUMIDITY']
-    ### Drop the old table.
-    #svp2Job.drop('Time', axis = 1, inplace = True)
+    
+    #import the room reference data 
+    roomData = pd.read_csv(filePath/'RIC_ROOM_REFERENCE-TempRH--.csv', skiprows=range(1,140000), parse_dates=['Date/Time'])
+    svp2Job['roomDateTime'] = roomData['Date/Time']
+    svp2Job['roomTime'] = roomData['Date/Time'].astype('int64')//1e9- jobStartTime
+    svp2Job['roomTemp'] = (roomData['Temperature (F)'] -32) * (5/9)
+    svp2Job['roomHumidity'] = roomData['Moisture (%)']
+    
     #write out new table
     svp2Job.to_csv(filePath/'syncedData.csv', index = False)
     pass
 
 if __name__ == "__main__":
     #start time in unix timestamp is the first part of the title of the xl sheet
-    jobStartTime = 1656605942
+    jobStartTime = 1658857168
     #File path will need to be updated for each folder of data
-    dataFolder = '30Jun'
+    dataFolder = '26Jul'
     filePath = filePath = Path(r'C:\Users\TonyWitt\OneDrive - Evolve\Documents\Flir Logs')
     filePath = filePath/dataFolder
     timeSync(jobStartTime, filePath)
